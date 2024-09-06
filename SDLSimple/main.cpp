81c6f01411288bf568edadbea9c36c1fc7b98cb6
yawnka <ys4780@nyu.edu>
2024-09-06 16:12:18 -0400

    Initial Commit

---
+++ SDLSimple/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  SDLSimple
+//
+//  Created by Yanka Sikder on 9/6/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
